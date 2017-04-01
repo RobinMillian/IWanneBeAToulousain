91acea55175b7f2468b683a599eeea8810669ff8
RobinMillian <robin.millian@epitech.eu>
2017-04-01 02:30:15 +0200

    Models3D

---
+++ Assets/FurBall2D/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform target;
+    public float padding;
+    public bool camFollowUp;
+
+	public void Start()
+	{
+	}
+
+	public void Update()
+	{
+        if (!camFollowUp)
+            transform.Translate(target.position.x - transform.position.x, 0, 0);
+        else
+            transform.Translate(target.position.x - transform.position.x, transform.position.y + padding - transform.position.y, 0);
+	}
+}
---
+++ Assets/FurBall2D_mobile/Scripts/PlayerController_m.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+public class PlayerController_m : MonoBehaviour {
+	
+	public float maxSpeed = 6f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	public float verticalSpeed = 20;
+	[HideInInspector]
+	public bool lookingRight = true;
+	bool doubleJump = false;
+	public GameObject Boost;
+//	private Animator cloudanim;
+	public GameObject Cloud;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+	private bool isGrounded = false;
+    private float speed_player = 0;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		Cloud = GameObject.Find("Cloud");
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D collision2D) {
+		
+		if (collision2D.relativeVelocity.magnitude > 20){
+			Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+		}
+	}
+	
+	void Update () {
+
+		if (CrossPlatformInputManager.GetButtonDown ("Jump") && (isGrounded || !doubleJump))
+		{
+			rb2d.AddForce(new Vector2(0,jumpForce));
+
+			if (!doubleJump && !isGrounded)
+			{
+				doubleJump = true;
+				Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+			//	cloudanim.Play("cloud");		
+			}
+		}
+
+
+
+		if (CrossPlatformInputManager.GetButtonDown("Boost") && !isGrounded)
+		{
+			rb2d.AddForce(new Vector2(0,-jumpForce));
+			Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+		}
+
+	}
+
+
+	void FixedUpdate()
+	{
+		if (isGrounded) 
+			doubleJump = false;
+
+
+        //		float hor = CrossPlatformInputManager.GetAxis ("Horizontal");
+        speed_player += 0.5f * Time.deltaTime;
+        if (speed_player > maxSpeed)
+            speed_player = maxSpeed;
+
+        anim.SetFloat ("Speed", Mathf.Abs (speed_player));
+
+		rb2d.velocity = new Vector2 (speed_player * maxSpeed, rb2d.velocity.y);
+		  
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, 0.15F, whatIsGround);
+
+		anim.SetBool ("IsGrounded", isGrounded);
+
+		if ((speed_player > 0 && !lookingRight)||(speed_player < 0 && lookingRight))
+			Flip ();
+		 
+		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+
+	
+	public void Flip()
+	{
+		lookingRight = !lookingRight;
+		Vector3 myScale = transform.localScale;
+		myScale.x *= -1;
+		transform.localScale = myScale;
+	}
+
+}
